a6b4cb4c3c06f290b7596ad89d13ab59370d2914
Pedro Lago <159453815+PedroLagoX@users.noreply.github.com>
2025-09-19 11:19:28 -0300

    Add files via upload

---
+++ qrcode/js/script.js
@@ -0,0 +1,31 @@
+
+
+const url = document.querySelector('#url')
+const botao = document.querySelector('#botao')
+const qr = document.querySelector('#qrcode')
+
+// eventos
+
+botao.addEventListener('click', gerador)
+url.addEventListener('keydown', (event)=>{
+    if(event.key == 'Enter'){
+        gerador()
+    }
+})
+
+function gerador(){
+    texto = url.value
+    if(texto){
+        qr.innerHTML = ''
+        code = new QRCode(qr, {
+            text:texto,
+            width:300,
+            height:300,
+            colorDark: 'rgba(0, 0, 0, 0)',
+            colorLight:'black'
+        }
+        )
+    }else{
+        qr.innerHTML = ''
+    }
+}
